4ddae57564376bc3378de9cb992cced0566d6e23
Superlllas2 <64853531+Superlllas2@users.noreply.github.com>
2024-11-07 01:24:46 +0100

    initial commit

--- server.js
+++ server.js
@@ -48,13 +48,13 @@ if (process.env.MODE === 'development') {
 //     }
 //     next()
 // })
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-});
+//
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "https://thequicksport.com");
+//     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//     next();
+// });
 
 // Middleware to parse JSON
 app.use(express.json());
